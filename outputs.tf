6db3fd6eb66cb4c7c7b906e8bf12ff1eb1811c2c
rhysj6 <rhysj6@gmail.com>
2025-03-26 22:04:47 +0000

    Added output descriptions

--- outputs.tf
+++ outputs.tf
@@ -1,11 +1,14 @@
 output "cluster_exists" {
   value = data.rancher2_cluster_v2.cluster.id != null
+  description = "Whether the cluster has been found"
 }
 
 output "host" {
     value = local.kubeconfig != null ? local.kubeconfig.clusters[0].cluster.server : null
+    description = "The host of the downstream cluster, which is the Rancher server URL. This will be null if the cluster is not ready."
 }
 
 output "token" {
     value = local.kubeconfig != null ? local.kubeconfig.users[0].user.token : null
+    description = "The token of the downstream cluster, which is used to authenticate with the Rancher server. This will be null if the cluster is not ready."
 }
